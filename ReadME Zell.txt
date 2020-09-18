be4228b06c2efe913791745428b4baeb970d586e
percevil <peeevil@gmail.com>
2020-09-18 14:29:40 -0700

    selectedRoles filter Vscode throwing syntax error

---
+++ ReadME Zell.txt
@@ -0,0 +1,12 @@
+Hey Zell,
+
+Take a look at the 2 screen shots that I provided.
+This is just one example of the problems of your learning sections that are unclear that Ive run into that have caused me many hours of further frustration.
+ Im not sure if this example this information is assumed that I new the material or I'm just being plain stupid.
+
+ Take a look, I'm not having a very good time with this material as the learning curve gets steeper and steeper.
+
+
+
+
+ Percy
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const heroesList = document.querySelector('.heroes-list');
 const filtersDiv = document.querySelector('.filters');
-
 const dotaApi = 'https://api.opendota.com/api';
 
 // zlFetch(`${dotaApi}/heroStats`)
@@ -22,7 +21,12 @@ zlFetch(`${dotaApi}/heroStats`).then(response => {
     heroesList.appendChild(li);
   });
 
+
+
+    // FILTERS
+    
   filtersDiv.addEventListener('change', event => {
+    // CHECKBOXES
     const selectedCheckboxes = [
       ...document.querySelectorAll('#attack-type input:checked'),
     ].map(checkbox => checkbox.id);
@@ -34,13 +38,51 @@ zlFetch(`${dotaApi}/heroStats`).then(response => {
     console.log(selectedAttackTypes);
     // listening for which type checkbox input was checked
 
-    const filtered = heroes.filter(hero => {
-      const attackType = hero.attack_type.toLowerCase();
-      // changing cases so input matches selection
-      return selectedAttackTypes.includes(attackType);
-      // if hero attack type is found, it will be included
-    });
+    const selectedPrimaryAttributes = [
+      ...document.querySelectorAll('#primary-attribute input:checked'),
+    ].map(checkbox => checkbox.id);
+    // get the selected primary attributes
 
+    const selectedRoles = [
+      ...document.querySelectorAll('#role input:checked'),
+    ].map(checkbox => checkbox.id);
+    // Listen for
+
+  
+
+    // attackType filter 12.37 (OLD)
+    // const filtered = heroes
+    // .filter(hero => {
+    //   const attackType = hero.attack_type.toLowerCase();
+    //   // changing cases so input matches selection
+    //   return selectedAttackTypes.includes(attackType);
+    //   // if hero attack type is found, it will be included
+    // });
+
+    // attackType filter 12.38. (NEW) Use this filter to match the other 2 filters
+    const filtered = heroes
+      .filter(hero => {
+        if (selectedAttackTypes.length === 0) return true;
+        const attackType = hero.attack_type.toLowerCase();
+        return selectedAttackTypes.includes(attackType);
+      })
+      // Primary attribute filter
+      .filter(hero => {
+        if (selectedPrimaryAttributes.length === 0) return true;
+        return selectedPrimaryAttributes.includes(hero.primary_attr);
+        // Checks if primary attribute was selected by user
+      });
+
+      .filter(hero => {
+        if (selectedRoles.length === 0) return true
+        const heroRoles = hero.roles.map(role => role.toLowerCase())
+        return selectedRoles.some(role => {
+          return heroRoles.includes(role)
+        })
+      })
+
+
+    // Diplaying new info from inputs
     heroesList.innerHTML = '';
     // remove the current list of heroes by setting .heroes-list's innerHTML to ''
     filtered.forEach(hero => {
@@ -51,7 +93,7 @@ zlFetch(`${dotaApi}/heroStats`).then(response => {
         <span class="hero__name"> ${hero.localized_name} </span>
         <img src="https://api.opendota.com${hero.img}" alt="${hero.localized_name} image">
       </a> `;
-      // creating the new li elements and updating the DOM list
+      // creating the new li elements and updating the DOM list after filtering from input
       heroesList.appendChild(li);
       // displaying the new list
 
